f367bda5f280a7449406bc83ab048dbb5a28502e
MoonBasedKas <kassidy.maberry@gmail.com>
2024-10-02 18:22:57 -0600

    Filled in more information

--- frame.py
+++ frame.py
@@ -1,7 +1,7 @@
 """
 Name: Kassidy Maberry
-Project:
-Date:
+Project: Star Network
+Date: 2024/11/01
 
 Implementation of the frame class. This is used as a container 
 for handling the information of a packet and for preparing 
